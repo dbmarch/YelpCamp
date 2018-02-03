d25ddc30d937f447d2946553dcd295c0aae5432b
EC2 Default User <ec2-user@ip-172-31-15-112.us-east-2.compute.internal>
2018-02-03 08:48:45 +0000

    edit and delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -51,6 +51,45 @@ router.post ('/', isLoggedIn, function (req, res) {
     })
 });
 
+//EDITE
+router.get ('/:comment_id/edit', function(req,res){
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
+            res.redirect ('back');
+            console.log(err)
+        } else {
+            res.render ('comments/edit', {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+
+// UPDATE
+router.put('/:comment_id', function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, foundComment) {
+        if(err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id );
+            // res.redirect('/:' + req.params.id);
+        }
+    });
+})
+
+
+
+// DESTROY
+router.delete ('/:comment_id', function (req,res) {
+    console.log ('delete comment');
+    
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+          if(err){
+              res.redirect('back');
+          } else {
+              res.redirect('/campgrounds/'+req.params.id);
+          }
+    })
+})
 
 function isLoggedIn(req,res,next){
     if (req.isAuthenticated()) {
